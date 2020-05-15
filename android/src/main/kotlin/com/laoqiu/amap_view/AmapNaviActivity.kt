ed1432788b7a336202ed0b19ee328b484f10023a
Spicely <Spicely@outlook.com>
2020-05-14 19:25:16 -0700

    []

--- android/src/main/kotlin/com/laoqiu/amap_view/AmapNaviActivity.kt
+++ android/src/main/kotlin/com/laoqiu/amap_view/AmapNaviActivity.kt
@@ -22,24 +22,22 @@ class AmapNaviActivity: Activity(), AMapNaviListener, AMapNaviViewListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState != null) {
-            setContentView(R.layout.activity_basic_navi)
-            Log.e("调试信息", "当前为导航页面")
-            val bundle = this.intent.extras
-            var startLat = bundle.getDouble("startLat")
-            var startLng = bundle.getDouble("startLng")
-            if (startLat != 0.0 && startLng != 0.0) {
-                startLatLng = NaviLatLng(startLat, startLng)
-            }
-            endLatLng = NaviLatLng(bundle.getDouble("endLat"),bundle.getDouble("endLng"))
-            naviType = bundle.getInt("naviType")
-            mAMapNaviView = findViewById<View>(R.id.navi_view) as AMapNaviView
-            mAMapNaviView?.onCreate(savedInstanceState)
-            mAMapNaviView?.setAMapNaviViewListener(this)
-            mAMapNavi = AMapNavi.getInstance(applicationContext)
-            mAMapNavi?.setUseInnerVoice(true)
-            mAMapNavi?.addAMapNaviListener(this)
+        setContentView(R.layout.activity_basic_navi)
+        Log.e("调试信息", "当前为导航页面")
+        val bundle = this.intent.extras
+        var startLat = bundle.getDouble("startLat")
+        var startLng = bundle.getDouble("startLng")
+        if (startLat != 0.0 && startLng != 0.0) {
+            startLatLng = NaviLatLng(startLat, startLng)
         }
+        endLatLng = NaviLatLng(bundle.getDouble("endLat"),bundle.getDouble("endLng"))
+        naviType = bundle.getInt("naviType")
+        mAMapNaviView = findViewById<View>(R.id.navi_view) as AMapNaviView
+        mAMapNaviView?.onCreate(Bundle())
+        mAMapNaviView?.setAMapNaviViewListener(this)
+        mAMapNavi = AMapNavi.getInstance(applicationContext)
+        mAMapNavi?.setUseInnerVoice(true)
+        mAMapNavi?.addAMapNaviListener(this)
     }
     override fun onResume() {
         super.onResume()
